23c65c9fbbfa713a2312204d59fdec91ae062fd1
nikitamamaev <nikratos@yandex.ru>
2021-03-03 23:59:39 +0300

    feat: Update User struct.

--- models/users.go
+++ models/users.go
@@ -1,7 +1,8 @@
 package models
 
 type User struct {
-	ID   uint   `json:"id" gorm:"primary_key"`
-	Name string `json:"name"`
-	Book string `json:"book"`
+	ID       uint   `json:"id" gorm:"primary_key"`
+	Name     string `json:"name"`
+	Book     string `json:"book"`
+	Password string `json:"password"`
 }
